0949f0de2d313c236963062bd6f1eef302b7fb1b
rkrishna9 <49833401+rkrishna9@users.noreply.github.com>
2020-01-19 15:44:16 +0530

    commit-bucket

--- eks-demo/backend.tf
+++ eks-demo/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3"{
-    bucket = "qtterraformstatestorage"
+    bucket = "eks-cluster-rk"
     access_key = var.aceess-Key
     secret_key = var.secret-Key
     region = "us-east-1"
