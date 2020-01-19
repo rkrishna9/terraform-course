c5a8cd2911aacea0aff6707e62af1dafdf5ec1cf
rkrishna9 <49833401+rkrishna9@users.noreply.github.com>
2020-01-19 15:54:00 +0530

    variabl file was changed

--- eks-demo/variables.tf
+++ eks-demo/variables.tf
@@ -1,1 +1,8 @@
-ii
+variable "aceess-Key" {
+  description = "provide access-key"
+  type        = string
+}
+variable "secret-Key" {
+  description = "provide secret-key"
+  type        = string
+}
